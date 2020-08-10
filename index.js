7fad3cd21f989e150d2490e060bba0b2a2b38802
Jacob Ashworth <jeashwor@gmail.com>
2020-08-10 18:47:14 -0400

    began writing question array

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        name: "projectTitle",
+        message: "What is your project title?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Write a brief project description."
+    },
+    {
+        type: "input",
+        name: "installInstructions",
+        message: "List instructions for install of this application. (For multiple steps separate statements by commas)"
+    },
+    {
+        type: "input",
+        name: "usageInfo",
+        message: "Describe usage information."
+    },
+    {
+        type: "input",
+        name: "contributionGuidelines",
+        message: "Describe any contribution guidelines necessary for use of application."
+    },
+    {
+        type: "input",
+        name: "testInstructions",
+        message: "List test instructions for user as necessary. (For multiple steps separate statements by commas)"
+    },
+    {
+        type:
+        name:
+        message:
+    }
 ];
 
 // function to write README file
