fbfc6bb4d2f2510d70bc45c2e45d11f480c1bcb5
Jacob Ashworth <jeashwor@gmail.com>
2020-08-10 21:37:06 -0400

    created test.md file to begin generating md file headers and general styling

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,15 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+    ${data.description}
+    place screen shot here
+    ## Table of Contents
+    [Installation](#installation-instructions)
+    [Usage](#usage-instructions)
+    [License](#license)
+    [Contributing](#contributing)
+    [Test](#test-instructions)
+
 
 `;
 }
