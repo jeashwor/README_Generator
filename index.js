fa5388e1f4f5dda1582152a09c2ad0afc183a47f
Jacob Ashworth <jeashwor@gmail.com>
2020-08-10 23:04:20 -0400

    added inquirer function to init function

--- index.js
+++ index.js
@@ -3,59 +3,11 @@ var inquirer = require("inquirer");
 var fs = require('fs');
 
 // array of questions for user
-const questions = [
-    {
-        type: "input",
-        name: "projectTitle",
-        message: "What is your project title?"
-    },
-    {
-        type: "input",
-        name: "description",
-        message: "Write a brief project description."
-    },
-    {
-        type: "input",
-        name: "installInstructions",
-        message: "List instructions for install of this application. (For multiple steps separate statements by commas)"
-    },
-    {
-        type: "input",
-        name: "usageInfo",
-        message: "Describe usage information."
-    },
-    {
-        type: "input",
-        name: "contributionGuidelines",
-        message: "Describe any contribution guidelines necessary for use of application."
-    },
-    {
-        type: "input",
-        name: "testInstructions",
-        message: "List test instructions for user as necessary. (For multiple steps separate statements by commas)"
-    },
-    {
-        type: "list",
-        name: "license",
-        message: "Choose license to represent application.",
-        choices: [
-            "MIT",
-            "Apache-2.0",
-            "GNU GPLv3",
-            "GNU AGPLv3",
-        ]
-    },
-    {
-        type: "input",
-        name: "userName",
-        message: "Enter your github username"
-    },
-    {
-        type: "input",
-        name: "userEmail",
-        message: "Enter your email address."
-    }
-];
+const questions = ["What is your project title?", "Write a brief project description.", "List instructions for install of this application. (For multiple steps separate statements by commas)", "Describe usage information.", "Describe any contribution guidelines necessary for use of application.", "List test instructions for user as necessary. (For multiple steps separate statements by commas)", "Choose license to represent application.", , "Enter your github username", "Enter your email address."];
+
+const licenseChoices = ["MIT", "Apache-2.0", "GNU GPLv3", "GNU AGPLv3"];
+
+const fileName = "README.md";
 
 // const to store license choice results
 let userLicenseChoice = "";
@@ -67,7 +19,61 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "projectTitle",
+                message: questions[0]
+            },
+            {
+                type: "input",
+                name: "description",
+                message: questions[1]
+            },
+            {
+                type: "input",
+                name: "installInstructions",
+                message: questions[2]
+            },
+            {
+                type: "input",
+                name: "usageInfo",
+                message: questions[3]
+            },
+            {
+                type: "input",
+                name: "contributionGuidelines",
+                message: questions[4]
+            },
+            {
+                type: "input",
+                name: "testInstructions",
+                message: questions[5]
+            },
+            {
+                type: "list",
+                name: "license",
+                message: questions[6],
+                choices: [
+                    licenseChoices[0],
+                    licenseChoices[1],
+                    licenseChoices[2],
+                    licenseChoices[3],
+                ]
+            },
+            {
+                type: "input",
+                name: "userName",
+                message: "Enter your github username"
+            },
+            {
+                type: "input",
+                name: "userEmail",
+                message: "Enter your email address."
+                // Validate correct email format if you have time.
+            }
+        ])
 };
 
 // function call to initialize program
